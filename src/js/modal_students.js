11c054793d09377d963c51e93c83dcd3ab62b2ef
OlenaLotos <olenameshanych@gmail.com>
2022-06-30 14:52:36 +0300

    first doing modal students

---
+++ src/js/modal_students.js
@@ -0,0 +1,52 @@
+import { studentsData } from './students_arr';
+// import studentsTemplate from '../templates/modal_students.hbs';
+// import refs from './variables';
+// const {
+//   backdropStEl,
+//   listStudents,
+//   linkToDev,
+//   closeModalBtn,
+//   ...rest
+// } = refs;
+
+
+ const backdropStEl = document.querySelector('.backdrop--students')
+ const listStudents = document.querySelector('.team-items')
+ const linkToDev = document.getElementById('openModalLink')
+ const closeModalBtn = document.getElementById('closeModalStBtn')
+
+const studentItems = studentsData.map(studentsTemplate).join(' ');
+
+listStudents.insertAdjacentHTML('afterbegin', studentItems);
+
+
+
+const openLink = () => {
+    window.addEventListener('keydown', onKeyPress);
+    backdropStEl.classList.remove('is-hidden');
+};
+
+
+const closeModalStud = () => {
+    backdropStEl.classList.add('is-hidden');
+    window.removeEventListener('keydown', onKeyPress);
+};
+
+linkToDev.addEventListener('click', openLink);
+closeModalBtn.addEventListener('click', closeModalStud);
+backdropStEl.addEventListener('click', onBackdropClick);
+
+
+
+
+function onKeyPress(event) {
+  if (event.code === 'Escape') {
+    closeModalStud();
+  }
+}
+
+function onBackdropClick(event) {
+  if (event.target === event.currentTarget) {
+    closeModalStud();
+  }
+}
---
+++ src/js/students_arr.js
@@ -0,0 +1,48 @@
+
+  export const studentsData = 
+ [ {
+    "name": "1",
+    "foto_url": "",
+    "githab": "https://github.com/OlenaLotos",
+  },
+  {
+    "name": "2",
+    "foto_url": "",
+    "githab": "https://github.com/OlenaLotos",
+  },
+  {
+    "name": "3",
+    "foto_url": "",
+    "githab": "https://github.com/OlenaLotos",
+  },
+  {
+    "name": "4",
+      "foto_url": "",
+    "githab": "https://github.com/OlenaLotos",
+  },
+  {
+    "name": "5",
+    "foto_url": "",
+    "githab": "https://github.com/OlenaLotos",
+  },
+  {
+    "name": "6",
+    "foto_url": "",
+    "githab": "https://github.com/OlenaLotos",
+  },
+  {
+    "name": "7",
+    "foto_url": "",
+    "githab": "https://github.com/OlenaLotos",
+  },
+  {
+    "name": "8",
+    "foto_url": "",
+    "githab": "https://github.com/OlenaLotos",
+  },
+  {
+    "name": "9",
+    "foto_url": "",
+    "githab": "https://github.com/OlenaLotos",
+  },
+  ]
