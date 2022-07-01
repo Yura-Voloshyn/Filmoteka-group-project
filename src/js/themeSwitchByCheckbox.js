184a2ee0e0e9a951cd5e858d23d2dfae68e8ff74
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-07-01 16:29:06 +0300

    local host theme and simple animation

--- src/js/themeSwitchByCheckbox.js
+++ src/js/themeSwitchByCheckbox.js
@@ -1,20 +1,83 @@
 import { refs } from './refs';
-const toggler = document.querySelector('.toggler');
-// console.log(toggler);
 
-toggler.addEventListener('click', getCheckboxStatus);
+window.addEventListener('load', windowLoad);
 
-export function getCheckboxStatus() {
-  const chbox = document.querySelector('#checkbox');
-  //   console.log(chbox.checked);
-  //   //   console.log(chbox.checked);
-
-  if ((chbox.checked ^= 1)) {
-    document.querySelector('body').style.backgroundColor = 'black';
-    document.querySelector('.footer').style.backgroundColor = 'black';
-  } else {
-    document.querySelector('body').style.backgroundColor = '';
-    document.querySelector('.footer').style.backgroundColor = '';
+export function windowLoad() {
+  const toggler = document.querySelector('.toggler');
+  const chbox = document.querySelector('.theme-btn');
+  const htmlBlock = document.documentElement;
+  const saveUserTheme = localStorage.getItem('user-theme');
+  //   console.log(htmlBlock);
+  let userTheme;
+  if (window.matchMedia) {
+    userTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'light'
+      : 'dark';
   }
-  //   console.log(chbox.checked, 'done');
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', e => {
+      !saveUserTheme ? changeTheme() : null;
+    });
+  //     {userTheme = wilight
+  //       .matchMedia('(preferce-color-scheme: dark)')
+  //       .addEventListener('change', e => {
+  //         !saveUserTheme ? changeTheme() : null;
+  //       });
+  //   }
+  //
+  const asd = (chbox.checked ^= 1);
+  if (asd) {
+    chbox.addEventListener('click', function (e) {
+      changeTheme(true);
+    });
+  }
+  //   const asd = (chbox.checked ^= 1);
+  //   console.log(asd);
+  // if ((chbox.checked ^= 1)) {
+  //   chbox.addEventListener('click', function (e) {
+  //     localStorage.setItem('user-theme', '');
+  //   });
+  // }
+
+  function setThemeClass() {
+    if (saveUserTheme) {
+      htmlBlock.classList.add(saveUserTheme);
+    } else {
+      htmlBlock.classList.add(userTheme);
+    }
+  }
+  setThemeClass();
+
+  function changeTheme(saveTheme = false) {
+    let currentTheme = htmlBlock.classList.contains('light') ? 'light' : 'dark';
+    let newTheme;
+    if (currentTheme === 'light') {
+      newTheme = 'dark';
+    } else if (currentTheme === 'dark') {
+      newTheme = 'light';
+    }
+    htmlBlock.classList.remove(currentTheme);
+    htmlBlock.classList.add(newTheme);
+    saveTheme ? localStorage.setItem('user-theme', newTheme) : null;
+  }
 }
+
+// toggler.addEventListener('click', e => {
+//   console.log('clk');
+// });
+
+//   function getCheckboxStatus() {
+//   const chbox = document.querySelector('#checkbox');
+//   //   console.log(chbox.checked);
+//   //   //   console.log(chbox.checked);
+
+//   if ((chbox.checked ^= 1)) {
+//     document.querySelector('body').style.backgroundColor = 'black';
+//     document.querySelector('.footer').style.backgroundColor = 'black';
+//   } else {
+//     document.querySelector('body').style.backgroundColor = '';
+//     document.querySelector('.footer').style.backgroundColor = '';
+//   }
+//   //   console.log(chbox.checked, 'done');
+// }
