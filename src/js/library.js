ff8e90d607e3acf1257a21d629b8b4b46b0f869b
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2022-07-02 00:47:08 +0200

    render btns

--- src/js/library.js
+++ src/js/library.js
@@ -6,19 +6,25 @@ refs.libraryBtn = document.querySelector(".nav-btn");
 refs.library = document.querySelector('#library');
 refs.header = document.querySelector("header");
 refs.libraryHeader = document.querySelector('#library__header--tamplate');
+refs.headerContainer = document.querySelector('header > .container');
+
 console.log(refs.header);
 
 // refs.libraryBtn.addEventListner('click', showLibrary);
 
-function showLibrary() { 
+function showLibraryHeader() { 
     //cleans header and add new header
-    refs.header.innerHTML = "";
-    refs.header.append(refs.libraryHeader.content.cloneNode(true));
+    refs.form.classList.add('visually-hidden');
+    refs.header.classList.add('library__header');
+    refs.headerContainer.append(refs.libraryHeader.content.cloneNode(true));
+
 
     refs.mainMarkup.innerHTML = "";
     refs.mainMarkup.append(refs.library.content.cloneNode(true));
+
+
     //дістати з локал сторедж бібліотекку користувача
     // Джейсон парс і мепаємо розмітку
 
 }
-showLibrary();
+showLibraryHeader();
