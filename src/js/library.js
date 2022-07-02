d6326984a56054624536b73af6c93d53b6572e48
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2022-07-02 19:40:44 +0200

    fix switcher

--- src/js/library.js
+++ src/js/library.js
@@ -2,46 +2,58 @@ import { refs }  from "./refs";
 // import { MovieApiService } from './MovieApiService';
 
 
-refs.library = document.querySelector('#library');
 refs.header = document.querySelector("header");
-refs.libraryHeader = document.querySelector('#library__header--tamplate');
+refs.librarySwitcher = document.querySelector('.library__btn--wrapper');
 refs.headerContainer = document.querySelector('header > .container');
 refs.libraryBtn = document.querySelector(".nav-btn.library__btn");
 refs.homePageBtn = document.querySelector(".nav-btn.home__btn");
+refs.wachedBtn = document.querySelector(".wached");
 
 
 refs.libraryBtn.disabled = false;
+refs.homePageBtn.disabled = true;
 
-refs.libraryBtn.addEventListener('click', showLibrary);
+refs.headerContainer.append(refs.librarySwitcher);
+
+refs.libraryBtn.addEventListener('click', (e) => menuSwitcher(e));
+refs.homePageBtn.addEventListener('click', (e) => menuSwitcher(e));
+//тут треба додати слухача на кнопку логін
+
+function menuSwitcher(e) { 
+    switch (e.target.innerText) { 
+        case "HOME":
+            goToHomePage();
+            break;
+        case "MY LIBRARY":
+            showLibrary();
+            break;
+        //add case for login
+    }
+}
 
 function showLibrary() { 
     refs.libraryBtn.disabled = true;
+    refs.homePageBtn.disabled = false;
 
-    //cleans header and add new header
     refs.form.classList.add('visually-hidden');
     refs.header.classList.add('library__header');
 
-    refs.headerContainer.append(refs.libraryHeader.content.cloneNode(true));
-    refs.wachedBtn = document.querySelector(".wached");
+    refs.librarySwitcher.classList.remove('visually-hidden');
     refs.wachedBtn.classList.add('selected');
-
-
 
     // refs.mainMarkup.innerHTML = "";
     // refs.mainMarkup.append(refs.library.content.cloneNode(true));
 
-
     //дістати з локал сторедж бібліотекку користувача
     // Джейсон парс і мепаємо розмітку
-
 }
 
-refs.homePageBtn.addEventListener('click', goToHomePage);
 
 function goToHomePage() { 
+    refs.homePageBtn.disabled = true;
+    refs.libraryBtn.disabled = false;
+    
     refs.form.classList.remove('visually-hidden');
     refs.header.classList.remove('library__header');
-    refs.librarySwitch = document.querySelector(".library__btn--wrapper");
-    refs.librarySwitch.classList.add('visually-hidden')
-
+    refs.librarySwitcher.classList.add('visually-hidden');
 }
