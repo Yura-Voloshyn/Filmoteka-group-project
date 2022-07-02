39477418282ca00626691e3dca7a76d51e28b323
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2022-07-02 14:27:32 +0200

    adds navigation between home-page and Library

--- src/js/library.js
+++ src/js/library.js
@@ -1,30 +1,47 @@
-import { refs } from "./refs";
-import { MovieApiService } from './MovieApiService';
+import { refs }  from "./refs";
+// import { MovieApiService } from './MovieApiService';
 
 
-refs.libraryBtn = document.querySelector(".nav-btn");
 refs.library = document.querySelector('#library');
 refs.header = document.querySelector("header");
 refs.libraryHeader = document.querySelector('#library__header--tamplate');
 refs.headerContainer = document.querySelector('header > .container');
+refs.libraryBtn = document.querySelector(".nav-btn.library__btn");
+refs.homePageBtn = document.querySelector(".nav-btn.home__btn");
 
-console.log(refs.header);
 
-// refs.libraryBtn.addEventListner('click', showLibrary);
+refs.libraryBtn.disabled = false;
 
-function showLibraryHeader() { 
+refs.libraryBtn.addEventListener('click', showLibrary);
+
+function showLibrary() { 
+    refs.libraryBtn.disabled = true;
+
     //cleans header and add new header
     refs.form.classList.add('visually-hidden');
     refs.header.classList.add('library__header');
+
     refs.headerContainer.append(refs.libraryHeader.content.cloneNode(true));
+    refs.wachedBtn = document.querySelector(".wached");
+    refs.wachedBtn.classList.add('selected');
 
 
-    refs.mainMarkup.innerHTML = "";
-    refs.mainMarkup.append(refs.library.content.cloneNode(true));
+
+    // refs.mainMarkup.innerHTML = "";
+    // refs.mainMarkup.append(refs.library.content.cloneNode(true));
 
 
     //дістати з локал сторедж бібліотекку користувача
     // Джейсон парс і мепаємо розмітку
 
 }
-showLibraryHeader();
+
+refs.homePageBtn.addEventListener('click', goToHomePage);
+
+function goToHomePage() { 
+    refs.form.classList.remove('visually-hidden');
+    refs.header.classList.remove('library__header');
+    refs.librarySwitch = document.querySelector(".library__btn--wrapper");
+    refs.librarySwitch.classList.add('visually-hidden')
+
+}
