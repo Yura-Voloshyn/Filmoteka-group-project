89bb81ae8fd073b3f2bc59e54190027f5320e6b1
OlenaLotos <olenameshanych@gmail.com>
2022-07-03 16:44:30 +0300

    add photo and dsrk theme - doesnt work

--- src/js/modal_students.js
+++ src/js/modal_students.js
@@ -69,3 +69,5 @@ function onBackdropClick(event) {
     closeModalStud();
   }
 }
+
+
--- src/js/students_arr.js
+++ src/js/students_arr.js
@@ -7,22 +7,22 @@
   },
   {
     "name": "Sergiy Smaga",
-    "photo_url": "",
+    "photo_url": "https://drive.google.com/file/d/1mk4zJuO8H_6XZSvO5aJ1CBB6DuynZQ7V/view?usp=sharing",
     "githab": "https://github.com/sergiy-smaga",
   },
   {
     "name": "Olena Lotos",
-    "photo_url": "",
+    "photo_url": "https://drive.google.com/file/d/1S2RXKiW5nlxootMZsJ_-7DdoIDJhdvBr/view?usp=sharing",
     "githab": "https://github.com/OlenaLotos",
   },
   {
     "name": "Iryna Shashko",
-      "photo_url": "",
+      "photo_url": "https://drive.google.com/file/d/1VPowfnHtzM4Km8yYlPdL6W5hrsBqJ6LD/view?usp=sharing",
     "githab": "https://github.com/IrynaShashko",
   },
   {
     "name": "Yulia Pakhachuk",
-    "photo_url": "",
+    "photo_url": "https://drive.google.com/file/d/12GEWKP0qLLcx3BDfMh78htD2yyotqOMN/view?usp=sharing",
     "githab": "https://github.com/yuliapakhachuk",
   },
   {
@@ -37,12 +37,12 @@
   },
   {
     "name": "Vasylenko Myroslava",
-    "photo_url": "",
+    "photo_url": "https://drive.google.com/file/d/1L64HjCqgkYtP3oCgox5fa-lnImfzR-fp/view?usp=sharing",
     "githab": "https://github.com/MiraVV",
   },
   {
     "name": "Andrew Timoshkin",
-    "photo_url": "",
+    "photo_url": "https://drive.google.com/file/d/1oNk2dzUuS4F3ypkyxeZaKhIRHhQFecx2/view?usp=sharing",
     "githab": "https://github.com/andrewtimoshkin",
   },
   ]
