d5eec592a56f2eb0686486341abcb029c2166647
HannaMyrhorod <hanna.mirgorod@gmail.com>
2022-07-06 17:25:59 +0300

    Add Back To Top Button

---
+++ src/js/backToTopBtn.js
@@ -0,0 +1,28 @@
+import { refs } from './refs';
+
+const showOnPx = 300;
+
+const scrollContainer = () => {
+    return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+    document.body.scrollIntoView({
+        behavior: 'smooth'
+    });
+};
+
+export const onScrollFunction = () => {
+    const scrolledPercentage = (scrollContainer().scrollTop / (scrollContainer().scrollHeight - scrollContainer().clientHeight)) * 100;
+
+    refs.pageProgressBar.style.height = `${scrolledPercentage}%`;
+
+    if (scrollContainer().scrollTop > showOnPx) {
+        refs.backToTopBtn.classList.remove('is-hidden');
+    } else {
+        refs.backToTopBtn.classList.add('is-hidden');
+    };
+};
+
+document.onscroll = onScrollFunction;
+refs.backToTopBtn.addEventListener('click', goToTop);
