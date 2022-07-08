d8c41168b900041ed89ac3ff73cfac1a997432f2
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-07-08 11:54:46 +0300

    Make all gendres save in localstorage with 1 key

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // https://api.themoviedb.org/3/movie/550?api_key=b28dcafbfbdc99f3059a27aaeb93fed6
+
 import { windowLoad } from './js/themeSwitchByCheckbox';
 import renderTrendingPage from './js/renderTrendingPage';
 import { itemMarkup } from './js/renderTrendingPage';
 import { onFormSubmit } from './js/renderSearchResult';
 import { getCheckboxStatus } from './js/themeSwitchByCheckbox';
 import './js/library';
-import {renderWatchedLib} from './js/renderWatchedLib'
+
 import { onMovieCardClick } from './js/renderModal';
 
 
