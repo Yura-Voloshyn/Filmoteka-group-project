972bd18e63815007d093e802ce3ecb379da049f1
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-07-10 10:55:45 +0300

    adding language switch

---
+++ src/js/language/language-translate-static.js
@@ -0,0 +1,22 @@
+const languageTranslate = {
+  home: {
+    en: 'home',
+    uk: 'головна',
+  },
+  library: {
+    en: 'my library',
+    uk: 'бібліотека',
+  },
+  log: {
+    en: 'log in',
+    uk: 'увійти',
+  },
+  search: {
+    en: 'Movie search',
+    uk: 'Пошук фільмів',
+  },
+  library: {
+    en: 'my library',
+    uk: 'бібліотека',
+  },
+};
