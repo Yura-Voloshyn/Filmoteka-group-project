e46b99001926b6bc801ed7b91f7539fc7a3c39d8
IrynaShashko <irishkashashko@gmail.com>
2022-07-10 18:41:03 +0300

    add search btn

--- src/js/renderSearchResult.js
+++ src/js/renderSearchResult.js
@@ -3,20 +3,21 @@ import { refs } from './refs';
 import { onPaginateSearchBtnClick } from './paginationSearch';
 import { renderPaginationSearchBtn } from './paginationSearch';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import debounce from 'lodash.debounce';
+// import debounce from 'lodash.debounce';
 
 let singleGenre = [];
 const movieApiService = new MovieApiService();
-refs.input.addEventListener('input', debounce(onInputForm, 300));
+refs.form.addEventListener('submit', onFormSubmit);
+// refs.input.addEventListener('input', debounce(onInputForm, 600));
 import { loadAnimationAction } from './renderTrendingPage';
 
-export async function onInputForm(e) {
+ export async function onFormSubmit(e) {
   e.preventDefault();
   refs.homeBtn.disabled = false;
   refs.pagination.innerHTML = '';
   refs.paginationSearch.innerHTML = '';
   refs.mainMarkup.innerHTML = '';
-  movieApiService.query = e.target.value;
+  movieApiService.query = e.currentTarget.elements[0].value;
   if (movieApiService.query === '') {
     Notify.failure('input field cannot be empty.');
     return;
@@ -24,22 +25,27 @@ export async function onInputForm(e) {
   clearMarkup();
   movieApiService.resetPage();
   loadAnimationAction.classList.remove('is-hiden');
-  const input = e.target.value;
-  console.log('input', input);
-  movieApiService.query = input;
+  const input = e.currentTarget.elements[0].value;
+  console.log("input", input);
+   movieApiService.query = input;
+   console.log("movieApiService.query", movieApiService.query);
   const searchData = await movieApiService.fetchArticlesSearch(1);
 
-  console.log('movieApiService.query', movieApiService.query);
-  console.log('searchData', searchData);
+    console.log("movieApiService.query", movieApiService.query);
+    console.log("searchData", searchData);
   const searchMarkup = searchData.results
     .map(item => itemMarkupBySearch(item))
     .join('');
-  const max_page = searchData.total_pages;
-
-  renderPaginationSearchBtn(max_page);
-
+  const max_page = searchData.total_pages; 
+   if (movieApiService.query === '') {
+     Notify.failure('input field cannot be empty.');
+     return;
+   } else {
+     renderPaginationSearchBtn(max_page);
+   }
+  //   console.log(searchMarkup);
   loadAnimationAction.classList.add('is-hiden');
-
+  //   console.log(searchData.total_results);
   if (searchData.total_results === 0) {
     Notify.failure(
       'Sorry, there are no movies matching your search query. Please try again.'
@@ -48,7 +54,48 @@ export async function onInputForm(e) {
   }
   refs.paginationSearch.addEventListener('click', onPaginateSearchBtnClick);
   return refs.mainMarkup.insertAdjacentHTML('beforeend', searchMarkup);
+  
 }
+
+// export async function onInputForm(e) {
+//   e.preventDefault();
+//   refs.homeBtn.disabled = false;
+//   refs.pagination.innerHTML = '';
+//   refs.paginationSearch.innerHTML = '';
+//   refs.mainMarkup.innerHTML = '';
+//   movieApiService.query = e.target.value;
+//   if (movieApiService.query === '') {
+//     Notify.failure('input field cannot be empty.');
+//     return;
+//   }
+//   clearMarkup();
+//   movieApiService.resetPage();
+//   loadAnimationAction.classList.remove('is-hiden');
+//   const input = e.target.value;
+//   console.log('input', input);
+//   movieApiService.query = input;
+//   const searchData = await movieApiService.fetchArticlesSearch(1);
+
+//   console.log('movieApiService.query', movieApiService.query);
+//   console.log('searchData', searchData);
+//   const searchMarkup = searchData.results
+//     .map(item => itemMarkupBySearch(item))
+//     .join('');
+//   const max_page = searchData.total_pages;
+
+//   renderPaginationSearchBtn(max_page);
+
+//   loadAnimationAction.classList.add('is-hiden');
+
+//   if (searchData.total_results === 0) {
+//     Notify.failure(
+//       'Sorry, there are no movies matching your search query. Please try again.'
+//     );
+//     return;
+//   }
+//   refs.paginationSearch.addEventListener('click', onPaginateSearchBtnClick);
+//   return refs.mainMarkup.insertAdjacentHTML('beforeend', searchMarkup);
+// }
 
 export function clearMarkup() {
   refs.mainMarkup.innerHTML = '';
@@ -78,8 +125,12 @@ export function itemMarkupBySearch({
   title,
   genre_ids,
   release_date,
+  vote_average,
 }) {
-  if (poster_path === null) {
+  if (vote_average < 1) {
+    console.log('vote_average', vote_average);
+    return;
+  } else if (poster_path === null) {
     console.log('poster_path is null', poster_path);
     return;
   } else {
@@ -87,21 +138,19 @@ export function itemMarkupBySearch({
     return `
         <li class="movie-card" id="${id}">
   <a class="card-link" href="#"><img class="poster-image" src="https://image.tmdb.org/t/p/w342/${poster_path}" alt="${title}" loading="lazy" /></a>
-  
     <h2 class="card-title">
       ${title}
     </h2>
     <div class="info">
     <p class="info-item">
       ${genreEditForRender(
-        singleGenre.map(genre => genre.name),
-        2
-      )} 
+      singleGenre.map(genre => genre.name),
+      2
+    )} 
     </p>
     <p class="info-item info-item__date">| 
       ${release_date.slice(0, 4)}
     </p>
-    
   </div>
 </li>
       `;
