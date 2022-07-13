7cdd0f40c93ef022c77ad39b8e6c92c54c6b45d7
HannaMyrhorod <hanna.mirgorod@gmail.com>
2022-07-13 17:09:35 +0300

    Translate all Notify messages on Login modal

--- src/js/firebase/firebaseLogout.js
+++ src/js/firebase/firebaseLogout.js
@@ -7,8 +7,13 @@ const logOut = async (event) => {
   event.preventDefault();
     await signOut(auth);
     refs.logoutBtn.parentNode.classList.toggle('is-hidden');
-    refs.openLoginBtn.parentNode.classList.toggle('is-hidden');
-  Notify.info("You're successfully logged out.");
+  refs.openLoginBtn.parentNode.classList.toggle('is-hidden');
+  if (window.location.hash === '#en') {
+    Notify.info("You're successfully logged out.");
+  };
+  if (window.location.hash === '#uk') {
+    Notify.info('Ви вийшли з аккаунту.');
+  };
 };
 
 refs.logoutBtn.addEventListener('click', logOut);
