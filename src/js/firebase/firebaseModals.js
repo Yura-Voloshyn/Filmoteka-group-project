eb6c0d30c246c82ebdd4f84f85ec2592776cf7a7
HannaMyrhorod <hanna.mirgorod@gmail.com>
2022-07-13 18:36:36 +0300

    Add keys for login translation

--- src/js/firebase/firebaseModals.js
+++ src/js/firebase/firebaseModals.js
@@ -38,3 +38,37 @@ refs.modalBtn[1].addEventListener('click', switchModals);
 refs.openLoginBtn.addEventListener('click', openLoginModal);
 refs.closeLoginBtn.addEventListener('click', closeLoginModal);
 refs.loginBackdrop.addEventListener('click', onBackdropClick);
+
+
+
+// KEYS FOR LOGIN TRANSLATION
+
+// emailPlaceholder: {
+//     en: 'Email',
+//     uk: 'Введіть Email',
+//   },
+// passwordPlaceholder: {
+//   en: 'Password',
+//   uk: 'Введіть Пароль',
+// },
+// repeatPlaceholder: {
+//   en: 'Repeat Password',
+//   uk: 'Повторіть Пароль',
+// },
+// registerBtn: {
+//   en: 'Register',
+//   uk: 'Реєстрація',
+// },
+// loginBtn: {
+//   en: 'Login',
+//   uk: 'Увійти',
+// },
+// account: {
+//   en: 'Have an account?',
+//   uk: 'Вже є аккаунт?',
+// },
+// noAccount: {
+//   en: 'Don`t have an account?',
+//   uk: 'Немає облікового запису?',
+// },
+
