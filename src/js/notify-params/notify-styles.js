04d210c391007a84fe16168e195c843eda68d4a0
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-07-14 10:01:01 +0300

    додав фон з прозорістю для нотифікацій

--- src/js/notify-params/notify-styles.js
+++ src/js/notify-params/notify-styles.js
@@ -5,18 +5,18 @@ Notify.init({
   timeout: 3000,
   distance: '15px',
 
-  width: '320px',
+  width: '300px',
   fontSize: '14px',
   cssAnimationStyle: 'from-top',
   cssAnimationDuration: 600,
 
   failure: {
-    background: '#11ffee00',
+    background: 'rgba(0,0,0,0.7)',
     textColor: '#FF6B08',
     notiflixIconColor: 'rgba(200,0,0,0.4)',
   },
   success: {
-    background: '#11ffee00',
+    background: 'rgba(0,0,0,0.7)',
     textColor: '#32c682',
     notiflixIconColor: 'rgba(50,200,130,0.4)',
   },
