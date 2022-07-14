fda38c3d04b83f9dc166a656eb9f18deaf1f26bd
HannaMyrhorod <hanna.mirgorod@gmail.com>
2022-07-14 14:55:43 +0300

    Fixed Students Modal translation

--- src/js/modal_students.js
+++ src/js/modal_students.js
@@ -1,66 +1,60 @@
 import { studentsDataEn, studentsDataUk } from './students_arr';
 import { refs } from './refs';
-// import { languageTranslate } from './language/language-translate-static';
-import { modalTranslate } from './language/translateOnLangChange';
-import { studName } from './language/language-translate-static';
-import { possition } from './language/language-translate-static';
-import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import confetti from 'canvas-confetti';
 
-if (location.hash === '#en') {
-  const studentItemsEn = studentsDataEn
-    .map(({ photo_url, name, githab, possition }) => {
-      return `
-  <li class='team'>
-  <div class='card__tumb team__tumb'>
-    <img data-lang="studName" class='card__image team__image' src='${photo_url}' alt='${name}' />
-  </div>
-  <a href='${githab}' class='ref'>
-  <div class='ref__cover'>
-  <span class='ref__icon'></span>
-  <h3 data-lang="studName" class='ref__title'>${name}</h3>
-  
-   </div>
-  </a>
-<p data-lang="possition" class='ref__pos'>${possition}</p>
-</li>
-`;
-    })
-    .join('');
-}
-if (location.hash === '#en') {
-  let studentItemsUk = studentsDataUk
-    .map(({ photo_url, name, githab, possition }) => {
-      return `
-  <li class='team'>
-  <div class='card__tumb team__tumb'>
-    <img data-lang="studName" class='card__image team__image' src='${photo_url}' alt='${name}' />
-  </div>
-  <a href='${githab}' class='ref'>
-  <div class='ref__cover'>
-  <span class='ref__icon'></span>
-  <h3 data-lang="studName" class='ref__title'>${name}</h3>
-  
-   </div>
-  </a>
-<p data-lang="possition" class='ref__pos'>${possition}</p>
-</li>
-`;
-    })
-    .join('');
-  return studentItemsUk;
-}
+let defaultHash = location.hash === '#en' || location.hash === ''
+let studentItems;
 
-const lnk = document.querySelector('.footer-link');
+if (defaultHash) {
+  studentItems = studentsDataEn
+    .map(({ photo_url, name, githab, possition }) => {
+      return `
+  <li class='team'>
+  <div class='card__tumb team__tumb'>
+    <img data-lang="studName" class='card__image team__image' src='${photo_url}' alt='${name}' />
+  </div>
+  <a href='${githab}' class='ref'>
+  <div class='ref__cover'>
+  <span class='ref__icon'></span>
+  <h3 data-lang="studName" class='ref__title'>${name}</h3>
+  
+   </div>
+  </a>
+<p data-lang="possition" class='ref__pos'>${possition}</p>
+</li>
+`;
+    })
+    .join('');
+};
+
+if (location.hash === '#uk') {
+  studentItems = studentsDataUk
+    .map(({ photo_url, name, githab, possition }) => {
+      return `
+  <li class='team'>
+  <div class='card__tumb team__tumb'>
+    <img data-lang="studName" class='card__image team__image' src='${photo_url}' alt='${name}' />
+  </div>
+  <a href='${githab}' class='ref'>
+  <div class='ref__cover'>
+  <span class='ref__icon'></span>
+  <h3 data-lang="studName" class='ref__title'>${name}</h3>
+  
+   </div>
+  </a>
+<p data-lang="possition" class='ref__pos'>${possition}</p>
+</li>
+`;
+    })
+    .join('');
+};
+
 const renderModal = document.querySelector('.team-items');
 
 const openLink = () => {
   renderModal.innerHTML = '';
-  modalTranslate();
-  modalTranslateName();
-  modalTranslatePos();
-  refs.listStudents.insertAdjacentHTML('afterbegin', studentItemsUk);
+  refs.listStudents.insertAdjacentHTML('afterbegin', studentItems);
   window.addEventListener('keydown', onKeyPress);
   refs.backdropStEl.classList.remove('is-hidden');
   showConfetti();
@@ -85,126 +79,17 @@ function onKeyPress(event) {
   if (event.code === 'Escape') {
     closeModalStud();
   }
-}
+};
 
 function onBackdropClick(event) {
   if (event.target === event.currentTarget) {
     closeModalStud();
   }
-}
+};
 
-// function showConfetti() {
-//   confetti.create(document.getElementById('canvas'), {
-//     resize: true,
-//     useWorker: true,
-//   })({ particleCount: 100, spread: 160, zIndex: 2021 });
-// }
-
-function modalTranslateName() {
-  document.querySelectorAll('[data-lang]').forEach(el => {
-    for (const key in studName) {
-      if (key === el.dataset.lang) {
-        el.textContent =
-          location.hash === '#uk' ? studName[key].uk : studName[key].en;
-      }
-    }
-  });
-}
-
-function modalTranslatePos() {
-  document.querySelectorAll('[data-lang]').forEach(el => {
-    for (const key in possition) {
-      if (key === el.dataset.lang) {
-        el.textContent =
-          location.hash === '#uk' ? possition[key].uk : possition[key].en;
-      }
-    }
-  });
-}
-
-// import { studentsData } from './students_arr';
-
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import confetti from 'canvas-confetti';
-
-// // import studentsTemplate from '../templates/modal_students.hbs';
-// // import { refs } from './refs';
-
-// const backdropStEl = document.querySelector('.backdrop--students');
-// const listStudents = document.querySelector('.team-items');
-// const linkToDev = document.getElementById('openModalLink');
-// const closeModalBtn = document.getElementById('closeModalStBtn');
-// const closeModalBtnX = document.getElementById('closeModalStBtnX');
-
-// // const studentItems = studentsData.map(studentsTemplate).join(' ');
-// const studentItems = studentsData
-//   .map(({ photo_url, name, githab, possition }) => {
-//     return `
-//   <li class='team'>
-//   <div class='card__tumb team__tumb'>
-//     <img class='card__image team__image' src='${photo_url}' alt='${name}' />
-//   </div>
-//   <a href='${githab}' class='ref'>
-//   <div class='ref__cover'>
-//   <span class='ref__icon'></span>
-//   <h3 class='ref__title'>${name}</h3>
-
-//    </div>
-//   </a>
-// <p class='ref__pos'>${possition}</p>
-// </li>
-// `;
-//   })
-//   .join('');
-
-// //   new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: "alt"});
-// // console.log(studentItems);
-// const lnk = document.querySelector('.footer-link');
-// const renderModal = document.querySelector('.team-items');
-
-// const openLink = () => {
-//   renderModal.innerHTML = '';
-//   listStudents.insertAdjacentHTML('afterbegin', studentItems);
-//   window.addEventListener('keydown', onKeyPress);
-//   backdropStEl.classList.remove('is-hidden');
-//   showConfetti();
-// };
-
-// const closeModalStud = () => {
-//   backdropStEl.classList.add('is-hidden');
-//   window.removeEventListener('keydown', onKeyPress);
-
-// };
-
-// const closeModalStudX = () => {
-//   backdropStEl.classList.add('is-hidden');
-//   window.removeEventListener('keydown', onKeyPress);
-
-// };
-
-// linkToDev.addEventListener('click', openLink);
-// closeModalBtn.addEventListener('click', closeModalStud);
-// closeModalBtnX.addEventListener('click', closeModalStudX);
-// backdropStEl.addEventListener('click', onBackdropClick);
-
-// function onKeyPress(event) {
-//   if (event.code === 'Escape') {
-//     closeModalStud();
-
-//   }
-// }
-
-// function onBackdropClick(event) {
-//   if (event.target === event.currentTarget) {
-//     closeModalStud();
-
-//   }
-// }
-
-// function showConfetti() {
-//   confetti.create(document.getElementById('canvas'), {
-//     resize: true,
-//     useWorker: true,
-//   })({ particleCount: 100, spread: 160, zIndex: 2021 });
-// }
+function showConfetti() {
+  confetti.create(document.getElementById('canvas'), {
+    resize: true,
+    useWorker: true,
+  })({ particleCount: 100, spread: 160, zIndex: 2021 });
+};
