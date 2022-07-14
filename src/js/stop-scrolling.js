833d3b7e11320b2f948d7d95100cf8eced546ca1
HannaMyrhorod <hanna.mirgorod@gmail.com>
2022-07-14 15:27:49 +0300

    Delete needless code

--- src/js/stop-scrolling.js
+++ src/js/stop-scrolling.js
@@ -1,17 +1,9 @@
 export const scroll = {
-    disableScroll() {
-  document.body.classList.add("stop-scrolling");
-},
+  disableScroll() {
+    document.body.classList.add("stop-scrolling");
+  },
   
- enableScroll() {
-  document.body.classList.remove("stop-scrolling");
-},
-}
-
-// function disableScroll() {
-//   document.body.classList.add("stop-scrolling");
-// };
-  
-// function enableScroll() {
-//   document.body.classList.remove("stop-scrolling");
-// };
+  enableScroll() {
+    document.body.classList.remove("stop-scrolling");
+  },
+};
